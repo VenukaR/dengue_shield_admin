6361b53946c6672c01cfffe684bf5e542d911bf6
Venuka R Ranasinghe <124608919+VenukaR@users.noreply.github.com>
2025-08-16 14:04:38 +0530

    refactor(next.config): remove unused headers and experimental options

--- next.config.ts
+++ next.config.ts
@@ -2,24 +2,6 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  async headers() {
-    return [
-      {
-        // Apply these headers to all routes
-        source: '/(.*)',
-        headers: [
-          {
-            key: 'X-Frame-Options',
-            value: 'SAMEORIGIN',
-          },
-        ],
-      },
-    ];
-  },
-  // Allow external access
-  experimental: {
-    serverComponentsExternalPackages: [],
-  },
 };
 
 export default nextConfig;
